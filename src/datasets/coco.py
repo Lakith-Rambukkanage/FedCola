859fff1fc71662e71ef1aaafe1e1a8819c9a9d0f
Lakith Rambukkanage <aclakith@gmail.com>
2025-05-09 01:18:27 +1000

    Comment logs

--- src/datasets/coco.py
+++ src/datasets/coco.py
@@ -166,7 +166,7 @@ class CocoCaptionsCap(Dataset):
         elif self.modality == 'img' or self.modality == 'txt':
             # Handle missing image_id in iid_to_cls
             if image_id not in self.iid_to_cls:
-                logger.warning(f"Image ID {image_id} not found in iid_to_cls. Assigning default target.")
+                #logger.warning(f"Image ID {image_id} not found in iid_to_cls. Assigning default target.")
                 target = 0  # Default target
             else:
                 target = self.iid_to_cls[image_id]
